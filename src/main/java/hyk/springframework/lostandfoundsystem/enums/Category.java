a04d4261b82a1c2fe20032e05776c716aa7710eb
Yanant27 <htooyanant.hyk@gmail.com>
2022-05-16 18:09:27 +0630

    Add Category entity.

---
+++ src/main/java/hyk/springframework/lostandfoundsystem/enums/Category.java
@@ -0,0 +1,14 @@
+package hyk.springframework.lostandfoundsystem.enums;
+
+/**
+ * @author Htoo Yanant Khin
+ **/
+public enum Category {
+    ELECTRONIC,
+    DOCUMENT,
+    ACCESSORIES,
+    CLOTHING,
+    JEWELLERY,
+    FOOTWEAR,
+    OTHERS
+}
