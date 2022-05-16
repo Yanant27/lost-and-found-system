026a46ddde2e29051e339471c71c6c9ff4b11fb0
Yanant27 <htooyanant.hyk@gmail.com>
2022-05-16 18:07:39 +0630

    Add properties.

--- src/main/java/hyk/springframework/lostandfoundsystem/domain/LostFoundItem.java
+++ src/main/java/hyk/springframework/lostandfoundsystem/domain/LostFoundItem.java
@@ -1,7 +1,7 @@
 package hyk.springframework.lostandfoundsystem.domain;
 
+import hyk.springframework.lostandfoundsystem.enums.Category;
 import hyk.springframework.lostandfoundsystem.enums.Type;
-import hyk.springframework.lostandfoundsystem.enums.backup.Category;
 import lombok.*;
 import org.springframework.format.annotation.DateTimeFormat;
 
@@ -24,12 +24,16 @@ public class LostFoundItem extends BaseEntity {
     @Enumerated(EnumType.STRING)
     private Type type;
 
+    private String title;
+
 //    @Column(name = "lost_found_date")
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate lostFoundDate;
 
 //    @Column(name = "lost_found_location")
     private String lostFoundLocation;
+
+    private String description;
 
 //    @Column(name = "reporter_name")
     private String reporterName;
@@ -41,11 +45,14 @@ public class LostFoundItem extends BaseEntity {
 //    @Pattern(regexp = "^[09-]\\d{9}$")
     private String reporterPhoneNo;
 
-    @Embedded
+    @Enumerated(EnumType.STRING)
     private Category category;
 
     @Column(updatable = false)
     private String createdBy;
 
     private String modifiedBy;
+    
+    @ManyToOne
+    private Account account;
 }
