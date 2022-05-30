c77ed8e55898f066a5f3167a5b7e6eaa8477bb29
Yanant27 <htooyanant.hyk@gmail.com>
2022-05-30 18:42:01 +0630

    Update global exception handler.

--- src/main/java/hyk/springframework/lostandfoundsystem/exceptions/ControllerExceptionHandler.java
+++ src/main/java/hyk/springframework/lostandfoundsystem/exceptions/ControllerExceptionHandler.java
@@ -1,31 +1,57 @@
 package hyk.springframework.lostandfoundsystem.exceptions;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.HttpServletRequest;
+import java.time.LocalDateTime;
+
+/**
+ * @author Htoo Yanant Khin
+ */
 @Slf4j
-//@ControllerAdvice
+@ControllerAdvice
 public class ControllerExceptionHandler {
-//    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ExceptionHandler(NumberFormatException.class)
-    public ModelAndView handleBadRequest(Exception exception) {
-        log.error("Handling ControllerExceptionHandler");
+    @ExceptionHandler({NumberFormatException.class, IllegalArgumentException.class})
+    public ModelAndView handleBadRequest(HttpServletRequest req, Exception exception) {
+        log.error("Handling Bad Request Exception");
         log.error(exception.getMessage());
         ModelAndView modelAndView = new ModelAndView();
         modelAndView.setViewName("error/400badrequest");
         modelAndView.addObject("exception", exception);
+        modelAndView.addObject("timestamp", LocalDateTime.now());
+        modelAndView.addObject("url", req.getRequestURL());
+        modelAndView.addObject("status", HttpStatus.BAD_REQUEST);
         return modelAndView;
     }
 
-//    @ResponseStatus(HttpStatus.NOT_FOUND)
-//    @ExceptionHandler(RuntimeException.class)
-//    public ModelAndView handleNotFoundException(Exception exception) {
-//        log.error("Handling ControllerExceptionHandler");
-//        log.error(exception.getMessage());
-//        ModelAndView modelAndView = new ModelAndView();
-//        modelAndView.setViewName("error/404error");
-//        modelAndView.addObject("exception", exception);
-//        return modelAndView;
-//    }
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ModelAndView handleResourceNotFoundException(HttpServletRequest req, Exception exception) {
+        log.error("Handling Resource Not Found Exception");
+        log.error(exception.getMessage());
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("error/404notfound");
+        modelAndView.addObject("exception", exception);
+        modelAndView.addObject("timestamp", LocalDateTime.now());
+        modelAndView.addObject("url", req.getRequestURL());
+        modelAndView.addObject("status", HttpStatus.NOT_FOUND);
+        return modelAndView;
+    }
+
+    @ExceptionHandler(AccessDeniedException.class)
+    public ModelAndView handleAccessDeniedException(HttpServletRequest req, Exception exception) {
+        log.error("Handling Access Denied Exception");
+        log.error(exception.getMessage());
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("error/403forbidden");
+        modelAndView.addObject("exception", exception);
+        modelAndView.addObject("timestamp", LocalDateTime.now());
+        modelAndView.addObject("url", req.getRequestURL());
+        modelAndView.addObject("status", HttpStatus.FORBIDDEN);
+        return modelAndView;
+    }
 }
